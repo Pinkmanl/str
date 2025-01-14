6f38babaa2a90b7137b370c2473e1d7455cea82f
Pinkmanl <abduvohod23@gmail.com>
2025-01-14 23:12:21 +0500

    Add files via upload
    1,2-amaliy vazifa

---
+++ ternar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Tekshirish
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 15;
+            int y = 20;
+            string result = (x > y)
+             ? "x is greater then y"
+             : (x < y)
+               ? "x is less then y"
+               : (x == y)
+                 ? "x is equal to y"
+                 : "x and y are not comparable";
+            if (x > y)
+            {
+                result = "x is greater then y";
+                 }
+            else if (x < y)
+            {
+                result = "x is less then y";
+            }
+            else if (x == y)
+            {
+                result = "x is equal to y";
+            }
+            else 
+            {
+         result ="x and y are not comparable";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
