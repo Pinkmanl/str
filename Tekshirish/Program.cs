ac996992185cb0af8ef8e433bd925f22f0af8b70
Pinkmanl <abduvod23@gmail.com>
2025-01-14 21:01:39 +0500

    1-amaliy vazifa

---
+++ Tekshirish/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Tekshirish
+{
+    class Tekshirish
+    {
+        static void Main()
+        {
+            string str = "qwErTy";
+            int s = str.Length;
+            Console.WriteLine("strni  uzunligi = " + s);
+
+            Console.WriteLine("sonni kiriting");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (s < n)
+            {
+               Console.WriteLine($"katta harflarga o'girildi: { str.ToUpper()}");
+               
+            }
+            else
+            {
+               Console.WriteLine($"kichik harflarga o'girildi: { str.ToLower()}");  
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
